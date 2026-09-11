a3c0aa30ef465146ac2a2b0fc214ba692dec03d1
LIM JIN YU <2602974@sit.singaporetech.edu.sg>
2026-09-11 22:29:43 +0800

    Step 6 - 8

--- auditor.py
+++ auditor.py
@@ -1,4 +1,5 @@
-inventory = 0;
+inventory = 0
+failed = 0
 
 while True:
     stock = input("Enter Stock Quantity(or 'quit'): ")
@@ -9,14 +10,29 @@ while True:
 
     if stock.startswith("-") and stock[1:].isdigit():
         print("Stock cannot be a negative number!")
+        failed += 1
         continue
 
 
     if not stock.isdigit():
         print("Error! Please enter a valid number! ")
+        failed += 1
         continue
 
     stock = int(stock)
+
+
+    inventory += stock
+    print(f"Current Total Inventory: {inventory}")
+
+    if inventory > 500:
+        print("Stock exceeded 500! Please follow up! ")
+        break
+
+
+
+print(f"Total Inventory: {inventory}")
+print(f"Failed entries: {failed}")
 
 
 
